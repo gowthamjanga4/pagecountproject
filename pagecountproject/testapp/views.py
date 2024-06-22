59a282f2a31538be1686d0036ec410d7df2cdd8f
gowtham <gowthamjanga7@gmail.com>
2024-06-22 18:36:38 +0530

    pushing the page count project to git

---
+++ pagecountproject/testapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def count_view(request):
+    print('Cookies from the client:', request.COOKIES)
+    count = int(request.COOKIES.get('count', 0))
+    count = count + 1
+    response = render(request, 'testapp/count.html', {'count': count})
+    response.set_cookie('count', count)
+    return response
